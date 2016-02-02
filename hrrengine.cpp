b2948e50e24280b43242afa9d7ccb3a4161c2c8a
Grayson M Dubois <grayson@debby>
2016-02-01 20:49:34 -0600

    Set up the initial files for HRREngine

---
+++ hrrengine.cpp
@@ -0,0 +1,4 @@
+//  Program:		Holographic Reduced Representation Engine Implementation
+//  Filename:		hrrengine.cpp
+//  Author:		Grayson M. Dubois
+//  Mentor:		Dr. Joshua L. Phillips
---
+++ hrrengine.h
@@ -0,0 +1,15 @@
+//  Program:		Holographic Reduced Representation Engine Specification
+//  Filename:		hrrengine.h
+//  Author:		Grayson M. Dubois
+//  Mentor:		Dr. Joshua Phillips
+
+class HRREngine {
+
+private:
+	
+
+public:
+	// Generates an hrr representation for the given vector
+	void generateRepresentation(Vector<float>&);
+
+}
