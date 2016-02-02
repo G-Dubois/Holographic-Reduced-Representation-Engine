da01ff0802231669e752d66637a38112de63e0b7
root <root@debby>
2016-02-02 15:45:01 -0600

    Added basic vector generation to toolkit.
    Upload to work on from different workspace

--- hrrengine.cpp
+++ hrrengine.cpp
@@ -2,3 +2,20 @@
 //  Filename:		hrrengine.cpp
 //  Author:		Grayson M. Dubois
 //  Mentor:		Dr. Joshua L. Phillips
+
+#include<random>
+
+vector<float> HRREngine::generateHRR(int size) {
+	vector<float> vector(size);
+
+	std::random_device rd;
+	std::mt19937 e2(rd);
+
+	std::normal_distribution<float> dist(0, 1);
+
+	for (int i = 0; i < vector.size(); i++) {
+	
+	}
+
+	return
+}
--- main.cpp
+++ main.cpp
@@ -2,3 +2,28 @@
 // Created by Grayson on 2/1/2016.
 //
 
+#include <iostream>
+#include <vector>
+#include <hrr.h>
+
+using namespace std;
+
+int main (int argc, char** argv) {
+
+	int size;
+	vector<float> myVec;
+
+	cout << "What size vector do you want to create? ";
+	cin >> size;
+
+	myVec.resize(size);
+
+	HRREngine engine;
+	engine.generateHRR(myVec&);
+
+	for (int i = 0; i < myVec.size(); i++) {
+		cout << "|" << myVec[i] << "|\n";
+	}
+
+	return 0;
+}
